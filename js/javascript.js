8e81769f76ba0d43c2a557cc44b78babb13e0438
JoanneZamorano <108297574+JoanneZamorano@users.noreply.github.com>
2025-07-10 02:22:42 +0200

    cookies

--- js/javascript.js
+++ js/javascript.js
@@ -24,3 +24,16 @@ boton.addEventListener("click",oscuro);
 function oscuro(){
     document.body.classList.toggle("oscuro");
 }
+
+//COOKIES
+const popup = document.getElementById("cookiePopup");
+const btn = document.getElementById("acceptCookies");
+
+if (!localStorage.cookiesAccepted){
+    popup.classList.add("show");
+}
+
+btn.addEventListener("click",function(){
+    localStorage.cookiesAccepted = "true";
+    popup.classList.remove("show");
+});
